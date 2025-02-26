ff5bbac572f1d7bb0e0e8fa67ac212b7a03aa58d
Maxence-villet <maxence.villetschoumaker@gmail.com>
2025-02-26 11:23:44 +0100

    header and footer setup

---
+++ partials/footer.php
@@ -0,0 +1,12 @@
+
+        <div class="container-fluid">
+            <div class="copyright">
+                <p>Copyright &copy;2025</p>
+            </div>
+            <div class="contact">
+                <a href="https://github.com/Maxence-villet/Chess" class="github"><i class="fa-brands fa-github"></i></a> 
+                <a href="#" class="youtube"><i class="fa-brands fa-youtube"></i></a>           
+            </div>
+        </div>
+    </body>
+</html>
