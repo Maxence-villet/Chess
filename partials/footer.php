28f63d5d16c11ba4862f99af0db19c318ba770e6
Nicolas-77950 <nicolascheel77@gmail.com>
2025-02-27 22:18:15 +0100

    fix css of chat

--- partials/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <p>© 2025 Projet Chess</p>
-</footer>
-<link rel="stylesheet" href="/ProjetChess/Chess/public/css/footer.css">
-</body>
-</html>
--- partials/header.php
+++
@@ -1,6 +0,0 @@
-<header>
-    <h1>Projet Chess</h1>
-    <nav>
-        <a href="/ProjetChess/Chess/index.php">Accueil</a>
-    </nav>
-</header>
